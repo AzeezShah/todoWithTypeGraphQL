d04802b86156039c30a7b0a424f820defb1aa3d5
Azeez Shah <AzeezShah@192.168.1.8>
2023-01-03 17:54:46 +0530

    GraphQL server setup with Apollo and TypeGraphQL

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import "reflect-metadata";
 import express, { Express } from "express";
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import { TaskResolver } from "./resolvers/task";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 
 const main = async () => {
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [TaskResolver],
+            validate: false
+        }),
+        plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+    })
+    await apolloServer.start();
+
     const app: Express = express();
+
+    apolloServer.applyMiddleware({app});
 
     app.get("/", (_req, res) =>{
         res.send("Hello world!")
@@ -13,4 +29,3 @@ const main = async () => {
 }
 
 main().catch((err)=>console.error(err));
-
