52406ad0b68699d6755cf0d04f2f1aed2b221f96
Azeez Shah <AzeezShah@192.168.1.8>
2023-01-03 15:58:59 +0530

    Express setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # todoWithTypeGraphQL
+g
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello there.")
+import express, { Express } from "express";
+
+const main = async () => {
+    const app: Express = express();
+
+    app.get("/", (_req, res) =>{
+        res.send("Hello world!")
+    });
+
+    const PORT = process.env.PORT || 8000;
+
+    app.listen(PORT, ()=>console.log(`server started on port: ${PORT}`));
+}
+
+main().catch((err)=>console.error(err));
+
